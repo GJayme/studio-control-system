e53832d880188f24d8a29c37aa210a8ffa3b24b1
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-09-19 13:40:15 -0300

    feat: models Aluno

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Aluno
+
+
+@admin.register(Aluno)
+class ALunoAdmin(admin.ModelAdmin):
+    fields = ('name', 'date_of_birthday', 'telephone',
+              'email', 'cpf', 'gender', 'address')
--- students/models.py
+++ students/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+
 class Aluno(models.Model):
-  name = models.CharField(max_length=255)
-  
+    male = "Masculino"
+    female = "Feminino"
+    GENDER_CHOICES = [
+        (male, "Masculino"),
+        (female, "Feminino"),
+    ]
+
+    name = models.CharField(max_length=255)
+    date_of_birthday = models.DateField(auto_now_add=False, auto_now=False)
+    telephone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=255)
+    cpf = models.CharField(max_length=14, unique=True)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, default=female)
+    address = models.CharField(max_length=255)

a361c3a2d65f0f47aa5f5675a66d87e3a9052efd
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-09-19 19:19:39 -0300

    feat: models Anamnese

--- students/admin.py
+++ students/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Aluno
+from .models import Aluno, Anamnese
 
 
 @admin.register(Aluno)
-class ALunoAdmin(admin.ModelAdmin):
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'gender']
     fields = ('name', 'date_of_birthday', 'telephone',
               'email', 'cpf', 'gender', 'address')
+
+
+@admin.register(Anamnese)
+class Anamnese(admin.ModelAdmin):
+    list_display = ['weight', 'height', 'imc']
+    fields = ('weight', 'height', 'imc', 'has_pathology',
+              'pathology', 'use_medicine', 'medicine', 'has_surgery',
+              'surgery', 'heart_rate', 'objective', 'has_physical_activity',
+              'physical_activity', 'sleep_hours', 'has_smoker', 'meals_day')
--- students/models.py
+++ students/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+
 
 class Aluno(models.Model):
     male = "Masculino"
@@ -17,3 +19,58 @@ class Aluno(models.Model):
     gender = models.CharField(
         max_length=10, choices=GENDER_CHOICES, default=female)
     address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "O aluno {} foi cadastrado!".format(self.name)
+
+
+class Anamnese(models.Model):
+    wigth_loss = "Emagrecimento"
+    hypertrophy = "Hipertrofia"
+    muscle_strengthening = "Fortalecimento"
+    muscle_endurance = "Resistência"
+    muscle_strength = "Força"
+    OBJECTIVE_CHOICES = [
+        (wigth_loss, "Emagrecimento"),
+        (hypertrophy, "Hipertrofia"),
+        (muscle_strengthening, "Fortalecimento"),
+        (muscle_endurance, "Resistência"),
+        (muscle_strength, "Força")
+    ]
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    imc = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True)
+    has_pathology = models.BooleanField(default=False)
+    pathology = models.TextField(null=True, blank=True)
+    use_medicine = models.BooleanField(default=False)
+    medicine = models.TextField(null=True, blank=True)
+    has_surgery = models.BooleanField(default=False)
+    surgery = models.TextField(null=True, blank=True)
+    heart_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    objective = models.CharField(
+        max_length=20, choices=OBJECTIVE_CHOICES, default=wigth_loss)
+    has_physical_activity = models.BooleanField(default=False)
+    physical_activity = models.TextField(null=True, blank=True)
+    sleep_hours = models.DecimalField(max_digits=5, decimal_places=2)
+    has_smoker = models.BooleanField(default=False)
+    meals_day = models.IntegerField()
+
+    def __str__(self):
+        return "Anamnese cadastrada!"
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super(Anamnese, self).save(*args, **kwargs)
+
+    def clean(self):
+        try:
+            weight = self.weight
+            height = self.height
+            imc = (weight/(height)**2)*10000
+
+            self.imc = imc
+            print(self.imc)
+            super(Anamnese, self).clean()
+        except:
+            raise ValidationError("Erro ao calcular o IMC!")
